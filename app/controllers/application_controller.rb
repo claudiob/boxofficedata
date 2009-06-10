59792cc86eb40bd764576a32acb3ef8221b18baf
claudiob <claudiob@gmail.com>
2009-06-10 20:57:11 +0200

    rake is called with no path, to avoid problems on different machines.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,9 +9,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   def call_rake(task, options = {})
+    # From http://railscasts.com/episodes/127-rake-in-background
     options[:rails_env] ||= Rails.env
     args = options.map { |n, v| "#{n.to_s.upcase}='#{v}'" }
-    system "/usr/local/bin/rake #{task} #{args.join(' ')} --trace 2>&1 >> #{Rails.root}/log/rake.log &"
+    system "rake #{task} #{args.join(' ')} --trace 2>&1 >> #{Rails.root}/log/rake.log &"
   end
   
   # Scrub sensitive parameters from your log
