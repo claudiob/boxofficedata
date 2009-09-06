aea910a21728afc340765a0e7fc6a027e24ee711
claudiob <claudiob@gmail.com>
2009-09-06 08:31:42 +0200

    Added starts method to print the friday of a weekend.

--- app/helpers/weekends_helper.rb
+++ app/helpers/weekends_helper.rb
@@ -4,4 +4,10 @@ module WeekendsHelper
     sunday = friday + 2.days
     "#{friday.to_s} &mdash; #{sunday.to_s}"
   end
+
+  def starts(weekend)
+    friday = Date.civil(weekend.year,1,1).first_friday + (weekend.week-1).weeks
+    "#{friday.to_s}"
+  end
+
 end
